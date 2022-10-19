5f53f0c2a5901eef884c827605b072ab4c2ca46b
sadeemkhalaf <sadeem.kh992@gmail.com>
2022-10-19 10:00:30 +0300

    update api-constant and utils interval time

--- src/utils/api/api-constants.ts
+++ src/utils/api/api-constants.ts
@@ -1,3 +1,3 @@
 export const API_TIMEOUT = 10 * 1000; //10 sec
-export const DEFAULT_API = 'https://1af9-82-212-105-198.eu.ngrok.io';
+export const DEFAULT_API = 'https://c7c6-82-212-123-231.eu.ngrok.io';
 
